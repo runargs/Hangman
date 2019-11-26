d314d3ec13284da94a5b2e079fdb2dd532ec1f8c
runargs <39810066+runargs@users.noreply.github.com>
2019-11-25 23:47:50 -0500

    Move non-.java files out of src

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hangman
+
+A GUI hangman game including words from categories like names, movies, animals, food, countries, Pokemon, Game of Thrones, and astronomy
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -11,7 +11,6 @@
  */
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
